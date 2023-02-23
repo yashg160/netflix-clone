e645f9e6931c9ac104c68266ee22b02b9ecbb033
Yash Gupta <yashg160@gmail.com>
2023-02-24 00:18:09 +0530

    UPDATE: Fix Navbar fixed and banner placement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
+
 import Navbar from './components/Navbar/Navbar';
 import LandingBanner from './components/LandingBanner/LandingBanner';
+import LandingPageContent from './hocs/LandingPageContent/LandingPageContent';
 
 import './styles/globals.scss';
+import './styles/app.scss';
 
 function App() {
+  const [landingBannerRef, setLandingBannerRef] = useState(null);
+
   return (
     <div className='App'>
       <Navbar />
-      <LandingBanner />
+      <LandingBanner passRefToParent={(ref) => setLandingBannerRef(ref)} />
+      <LandingPageContent landingBannerRef={landingBannerRef} />
     </div>
   );
 }
